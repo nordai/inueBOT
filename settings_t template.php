a6f24a194fcad124e2ca2798d8b7417f7abfca44
Nordai <sistemi@nordai.com>
2016-04-27 11:05:16 +0200

    Update settings_t template.php

--- settings_t template.php
+++ settings_t template.php
@@ -9,7 +9,7 @@ define('LOG_FILE', 'telegram.log');
 // DB BOT Telegram
 define('DB_GEO_HOST', "127.0.0.1");
 define('DB_GEO_PORT', "5432");
-define('DB_GEO_NAME', "geonue_bot");
+define('DB_GEO_NAME', "");
 define('DB_GEO_USER', "");
 define('DB_GEO_PASSWORD', "");
 
@@ -22,7 +22,7 @@ define('DB_ERR', "errore database POSTGIS");
 // DB TOPO
 define('DB_TOPO_HOST', "127.0.0.1");
 define('DB_TOPO_PORT', "5432");
-define('DB_TOPO_NAME', "geonue");
+define('DB_TOPO_NAME', "");
 define('DB_TOPO_USER', "");
 define('DB_TOPO_PASSWORD', "");
 
